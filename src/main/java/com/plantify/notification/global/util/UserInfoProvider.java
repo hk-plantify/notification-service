38dfcca9decee8ac6b44f55cdc6d2bd7877ebcd9
seunzu <0123suh@gmail.com>
2024-12-19 15:52:44 +0900

    feat: userInfoProvider 수정

--- src/main/java/com/plantify/notification/global/util/UserInfoProvider.java
+++ src/main/java/com/plantify/notification/global/util/UserInfoProvider.java
@@ -4,24 +4,29 @@ import com.plantify.notification.domain.dto.AuthUserResponse;
 import com.plantify.notification.global.exception.ApplicationException;
 import com.plantify.notification.global.exception.errorcode.AuthErrorCode;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 @Component
 public class UserInfoProvider {
 
-    public AuthUserResponse getUserInfo() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || authentication.getPrincipal() == null) {
-            throw new ApplicationException(AuthErrorCode.INVALID_TOKEN);
-        }
+    public Mono<AuthUserResponse> getUserInfo() {
+        return ReactiveSecurityContextHolder.getContext()
+                .map(context -> {
+                    Authentication authentication = context.getAuthentication();
+                    if (authentication == null || authentication.getPrincipal() == null) {
+                        throw new ApplicationException(AuthErrorCode.INVALID_TOKEN);
+                    }
 
-        Long userId = (Long) authentication.getPrincipal();
-        String role = authentication.getAuthorities().stream()
-                .findFirst()
-                .map(grantedAuthority -> grantedAuthority.getAuthority().replace("ROLE_", ""))
-                .orElse("UNKNOWN");
+                    Long userId = (Long) authentication.getPrincipal();
+                    String role = authentication.getAuthorities().stream()
+                            .findFirst()
+                            .map(grantedAuthority -> grantedAuthority.getAuthority().replace("ROLE_", ""))
+                            .orElse("UNKNOWN");
 
-        return new AuthUserResponse(userId, role);
+                    return new AuthUserResponse(userId, role);
+                });
     }
 }
--- src/main/java/com/plantify/notification/service/NotificationServiceImpl.java
+++ src/main/java/com/plantify/notification/service/NotificationServiceImpl.java
@@ -4,6 +4,7 @@ import com.plantify.notification.domain.dto.NotificationResponse;
 import com.plantify.notification.global.util.UserInfoProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Sinks;
@@ -17,12 +18,15 @@ private final Sinks.Many<NotificationResponse> sink = Sinks.many().multicast().o
     private final UserInfoProvider userInfoProvider;
 
     public Flux<NotificationResponse> subscribe() {
-        Long userId = userInfoProvider.getUserInfo().userId();
-        log.info("Subscribing userId: {}", userId);
-        return sink.asFlux()
-                .filter(response -> response.userId().equals(userId))
-                .doOnSubscribe(subscription -> log.info("User {} subscribed.", userId));
+        return ReactiveSecurityContextHolder.getContext()
+                .flatMapMany(securityContext -> {
+                    Long userId = (Long) securityContext.getAuthentication().getPrincipal();
+                    return sink.asFlux()
+                            .filter(response -> response.userId().equals(userId))
+                            .doOnSubscribe(subscription -> log.info("User {} subscribed.", userId));
+                });
     }
+
 
     public void sendToClient(NotificationResponse response) {
         log.info("Sending notification to userId: {}", response.userId());
